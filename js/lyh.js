cac4c05860f82c71974d0c03bc9f8157fa0eb033
LI SHUANG <809889839@qq.com>
2016-12-11 15:56:15 +0800

    javascript DOM操作
    javascript DOM操作-李炎恢

--- js/lyh.js
+++ js/lyh.js
@@ -1,76 +1,39 @@
-//DOM
 /* window.onload=function(){
-	var box=document.getElementById("a");
+	var box=document.getElementById("learn");
 	alert(box);
 } */
 
 /* window.onload=function(){
-	var box=document.getElementById('a');
+	var box=document.getElementById("learn");
 	alert(box.tagName);
 } */
 
 /* window.onload=function(){
-	var box=document.getElementById("a");
+	var box=document.getElementById("learn");
 	alert(box.innerHTML);
 } */
 
 /* window.onload=function(){
-	var box=document.getElementsByClassName('zd');
-	alert(box.innerHTML);
+	var box=document.getElementById("learn");
+	alert(box.id);
 } */
 
 /* window.onload=function(){
-	var box=document.getElementById("a");
-	alert(box.className);
+	var box=document.getElementById("learn");
+	alert(box.title);
 } */
 
 /* window.onload=function(){
-	var box=document.getElementById("a");
-	alert(box.bbb);		//自定义属性
+	var box=document.getElementById("learn");
+	alert(box.style);  //获取style属性对象
 } */
 
 /* window.onload=function(){
-	var box=document.getElementById("a");
-	box.innerHTML="改变什么东西什么";
-	alert(box.innerHTML);
-} */
-
-/* window.onload=function(){
-	var box=document.getElementById("zd").className;
-	alert(box);
-} */
-
-/* window.onload=function(){
-	var box=document.getElementsByTagName("p");
-	//alert(box.length);
-	alert(box[0])
-} */
-
-/* window.onload=function(){
-	var box=document.getElementsByTagName("li");
-	//alert(box.length);
-	alert(box[0])
-} */
-
-/* window.onload=function(){
-	var box=document.getElementsByTagName("p");
-	alert(box[0].innerHTML)
-} */
-
-/* window.onload=function(){
-	var body=document.getElementsByTagName("body");
-	alert(body[0].innerHTML)
-} */
-
-/* window.onload=function(){
-	var box=document.getElementById("biaoqian");
-	alert(box.nodeName);		//获取元素节点的标签名，和tagName等价
+	var box=document.getElementById("learn");
+	alert(box.style.color);  //获取style属性对象中color属性的值
 } */
 
 window.onload=function(){
-	var box=document.getElementById("biaoqian");
-	alert(box.nodeValue);
+	var box=document.getElementById("learn");
+	alert(box.className);
 }
-
-
-
