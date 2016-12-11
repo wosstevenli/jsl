2a90ccbe831d3bcb1e1a25f1dd10ef24467879e2
LI SHUANG <809889839@qq.com>
2016-12-11 17:51:06 +0800

    学习DOM2
    学习DOM2

--- js/lyh.js
+++ js/lyh.js
@@ -33,7 +33,127 @@
 	alert(box.style.color);  //获取style属性对象中color属性的值
 } */
 
-window.onload=function(){
+/* window.onload=function(){
 	var box=document.getElementById("learn");
 	alert(box.className);
-}
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.innerHTML="缘分<strong>一道桥</strong>";  //赋值，可以解析HTML，不仅仅是单纯文本
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.id="3K";
+	alert(box.id);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByTagName("li");
+	alert(box.length);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByTagName("li");
+	alert(box.length);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByTagName("li");
+	alert(box.item());
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByTagName("li");
+	alert(box[0].tagName);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByTagName("li");
+	alert(box[0].innerHTML);
+} */
+
+/* window.onload=function(){
+	var body=document.getElementsByTagName("body");
+	alert(body);
+} */
+
+/* window.onload=function(){
+	var body=document.getElementsByTagName("body")[0];
+	alert(body);
+} */
+
+/* window.onload=function(){
+	var all=document.getElementsByTagName("*");
+	alert(all[0].tagName);
+} */
+
+/* window.onload=function(){
+	var all=document.getElementsByTagName("*");
+	alert(all.length);
+} */
+
+/* window.onload=function(){
+	var all=document.getElementsByTagName("*");
+	alert(all[21].tagName);
+} */
+
+/* window.onload=function(){
+	var all=document.getElementsByTagName("*");
+	alert(all[21].innerHTML);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByName("keywords")[1];
+	alert(box.innerHTML);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByName("keywords");
+	alert(box.length);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementsByName("keywords");
+	alert(box[0].tagName);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	alert(box.getAttribute("class"));
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	alert(box.getAttribute("className"));
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	if (box.getAttribute(className)==null){
+		alert(box.getAttribute("class"));
+	}else{
+		alert(box.getAttribute("className"));
+	} */
+	
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.setAttribute("title","标题");
+	alert(box.title);
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.setAttribute("align","center");
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.setAttribute("style","color:#3079ed");
+} */
+
+/* window.onload=function(){
+	var box=document.getElementById("learn");
+	box.removeAttribute("style");
+} */
